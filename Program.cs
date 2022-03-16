60d2360be1969c37d58e7ed47f719771edf03711
jzhguz <ljhbkh@hgvh.ooo>
2022-03-16 09:25:54 +0100

    Projektdateien hinzufügen.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stunde2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Stunde_1
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+           
+        }
+        }
+    }
+}
+    }
+}
